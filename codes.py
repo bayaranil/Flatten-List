25a22da819f308df8fe7b9ab28cb22526162091d
Anıl Bayar <75955104+bayaranil@users.noreply.github.com>
2022-11-28 11:56:17 +0300

    Create codes.py

---
+++ codes.py
@@ -0,0 +1,11 @@
+l = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+flatten_List = []
+
+def flatten(l):
+    for _ in l:
+        if type(_) == list:
+            flatten(_)
+        else:
+            flatten_List.append(_)
+    return flatten_List
+print(flatten(l))
